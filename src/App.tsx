8868f1d38cddaea33a89d5b093fdedc69071e001
shakib522 <shakibhasann522@gmail.com>
2025-02-09 00:05:46 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import ListGroup from "./component/ListGroup";
+function App(){
+  return <div><ListGroup></ListGroup></div>
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message(){
+    // JSX: Javascript XML
+    const name='Sakib';
+    if(name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+}
+export default Message;
